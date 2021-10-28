050a95f687e4cd751efe5216151dd7bff6bf97ee
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-10-27 20:51:24 -0500

    Datos nuevos 80,99

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 addEventListener("DOMContentLoaded", async()=>{
     let data = {
-        numero1: 10,
-        numero2: '8'
+        numero1: 80,
+        numero2: '99'
     };
     let peticio = await fetch("https://pruebacofenalco.000webhostapp.com/Formulario_Operadores_Comparacion2_27_10_2021/api.php", {method: "POST", body: JSON.stringify(data)});
     let json = await peticio.text();
